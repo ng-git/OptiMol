74dcb6352f1410ca08ca1d5636cb7945ea2fd9e9
minh <minh4829@gmail.com>
2020-03-02 22:46:00 -0800

    packages for smiles updated

--- SMILES_convert_MN.py
+++ SMILES_convert_MN.py
@@ -1,3 +1,11 @@
 from openeye import oechem
+from chemspipy import ChemSpider
+import openbabel
 
-test = 'CH3-CH3'
+# test = 'CH3-CH3'
+# cs = ChemSpider('d0xQqfSr9KAwCQDMb10uAmp46dAADGqh')
+# c = cs.get_compound(2157)
+#
+# a = c.mol_2d
+#
+# print(c.mol_2d)
