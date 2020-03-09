868114d153aec7302cf4475733ea2a49f3f33a5c
Minh Nguyen <n01@uw.edu>
2020-03-09 14:42:25 -0700

    text read file in progress..

--- data_csv_compile.py
+++ data_csv_compile.py
@@ -2,8 +2,20 @@ import pandas as pd
 import numpy as np
 
 
-# get the number of atoms to cut off the text rows
-atom_amount = int(pd.read_csv('aspirin_2d.txt', nrows=1).iloc[0, 0][1:3])
+# get the number of atoms to in 2D cut off the text rows
+atom_amount = int(pd.read_csv('aspirin_2d.txt', skiprows=1).iloc[0, 0][1:3])
+print(atom_amount)
+# get the dataframe for 2d coord
+coord_2d = pd.read_csv('aspirin_2d.txt', skiprows=2, nrows=atom_amount)
+# get the bonding for 2d coord
+bond_2d = pd.read_csv('aspirin_2d.txt', skiprows=3+atom_amount, nrows=atom_amount)
 
-data_2d = pd.read_csv('aspirin_2d.txt', nrows=atom_amount)
+
+# get the number of atoms in 3D to cut off the text rows
+atom_amount = int(pd.read_csv('aspirin_3d.txt', skiprows=1).iloc[0, 0][1:3])
+print(atom_amount)
+# get the dataframe for 2d coord
+coord_3d = pd.read_csv('aspirin_3d.txt', skiprows=2, nrows=atom_amount)
+# get the bonding for 2d coord
+bond_3d = pd.read_csv('aspirin_3d.txt', skiprows=3+atom_amount, nrows=atom_amount)
 
