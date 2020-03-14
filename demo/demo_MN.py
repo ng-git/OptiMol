97ca9fd179d07d427dc5de7426c040f1ee62cdb6
minh <minh4829@gmail.com>
2020-03-14 14:10:29 -0700

    demo updated

--- demo/demo_MN.py
+++ demo/demo_MN.py
@@ -3,8 +3,17 @@ import pandas as pd
 
 
 id_list = data_compile.get_id()
-[coord_2d, bond_2d, coord_3d, bond_3d] = data_compile.get_df_database(id_list[35])
 
-print(id_list[35])
-for item in [coord_2d, bond_2d, coord_3d, bond_3d]:
+# this give the dataframes from database using the id
+data = data_compile.get_df_database(id_list[34])
+for item in data:
     print(item)
+
+# this give out a combined dataframe to be used for ML
+data_set = data_compile.get_all_dataset(set1=3)
+print(data_set)
+
+# this make the dataframe from user input
+user_set = data_compile.get_df_user(['user.txt'])
+print(user_set)
+
